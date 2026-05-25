afaaf136bfbc3151d7531e36adef9311d7cdc47d
Kumar Kaushik <kumarkaushik871@gmail.com>
2026-05-25 07:44:24 +0530

    chore: remove development-only assets from main

--- Backend/tests/setup/env.setup.js
+++
@@ -1,5 +0,0 @@
-process.env.NODE_ENV = "test";
-process.env.SKIP_DB_CONNECTION = "true";
-process.env.JWT_SECRET = "test-jwt-secret-value-12345";
-process.env.JWT_REFRESH_SECRET = "test-refresh-secret-value-12345";
-process.env.STRIPE_MOCK_MODE = "true";
